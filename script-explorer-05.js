cc1bc340f5c096d149bf6a65aaf6e0657bce6d61
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-09-05 15:38:24 -0300

    Aplicando a lógica pt1

--- script-explorer-05.js
+++ script-explorer-05.js
@@ -1,9 +1,30 @@
 
+let tentativas = 0
 
 
 function handleClick(event){
     event.preventDefault()
+    
+    const numeroSorteado = Math.round(Math.random() * 10)
+    const inputNumber = document.querySelector("#inputNumber").value
+    
 
-    const inputNumber = document.querySelector("#inputNumber")
-    console.log(inputNumber.value)
+    tentativas ++
+
+    if (Number(inputNumber) != numeroSorteado) {
+        console.log(`
+        Seu chute ${inputNumber}
+        Número sorteado ${numeroSorteado}
+        Você errou ${tentativas}X!`)
+        
+    } else {
+        console.log(`
+        Seu chute ${inputNumber}
+        Número sorteado ${numeroSorteado}
+        Você acertou em ${tentativas}!`)
+
+        document.querySelector('.tela1').classList.add('oculto')
+        document.querySelector('.tela2').classList.remove('oculto')
+
+    }
 }
