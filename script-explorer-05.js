d2192bdf77772d752030777be06ea7f6cd5b8466
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-09-06 21:04:36 -0300

    Aplicando eventos e callbacks

--- script-explorer-05.js
+++ script-explorer-05.js
@@ -1,40 +1,65 @@
 
 let tentativas = 0
 
+const tela1 = document.querySelector('.tela1')
+const tela2 = document.querySelector('.tela2')
 
-function handleClick(event){
-    event.preventDefault()
+
+
+// função callback
+function sortearNumero(event){
+
+    event.preventDefault() // Não faça o padrão deste evento ( Evento do form )
     
     const numeroSorteado = Math.round(Math.random() * 10)
     const inputNumber = document.querySelector("#inputNumber").value
     
-
+    console.log(numeroSorteado, inputNumber)
+    
+    
+    
     tentativas ++
+
 
     if (Number(inputNumber) != numeroSorteado) {
 
         document.querySelector('.tela1 h2').innerHTML = `
         Você errou ${tentativas}X!
         `
-        console.log(`
-        Seu chute ${inputNumber}
-        Número sorteado ${numeroSorteado}
-        Você errou ${tentativas}X!`)
+        console.log(numeroSorteado, inputNumber)
         
-    } else {
-        console.log(`
-        Seu chute ${inputNumber}
-        Número sorteado ${numeroSorteado}
-        Você acertou em ${tentativas}!`)
+    } else {        
 
-        document.querySelector('.tela1').classList.add('oculto')
-        document.querySelector('.tela2').classList.remove('oculto')
+        tela1.classList.add('oculto')
+        tela2.classList.remove('oculto')
 
-        document.querySelector('.tela2 h2').innerHTML = `
-        Seu chute ${inputNumber}
+        document.querySelector('.tela2 h2').innerHTML = 
+        `
+        Você chutou ${inputNumber}!
         Número sorteado ${numeroSorteado}
-        Você acertou em ${tentativas}!
+        Você acertou em ${tentativas} tentativa(s)!
         `
 
     }
 }
+
+
+
+// capturando eventos do html (Dom)
+const btnJogar = document.querySelector('#btn-jogar')
+const btnVoltar = document.querySelector('#btn-voltar')
+
+
+
+// dois argumentos ( evento, e a ação)
+btnJogar.addEventListener('click', sortearNumero)
+
+// evento do botão voltar
+btnVoltar.addEventListener('click', function(){
+    tela1.classList.remove('oculto')
+    tela2.classList.add('oculto')
+    tentativas = 0
+    document.querySelector('.tela1 h2').innerHTML = ''
+    inputNumber.value = ""
+    
+})
